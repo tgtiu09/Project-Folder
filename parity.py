13c3bd4d9b9653e1715e08a64e5b17d78a9f380a
tgtiu09 <tgtiu09@gmail.com>
2024-10-02 03:00:48 -0400

    Update parity.py

--- parity.py
+++ parity.py
@@ -1,10 +1,6 @@
-# Write here you solution for the 01-parity exercise
 
-# Prompt the user to enter an integer number
 par = input ("Give me a number ")
 if int(par) % 2 == 0:
     print(par + " is even.")
 else:
     print(par + " is odd.")
-
-# Determine if the number is even or odd
